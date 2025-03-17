83aa19505c92057ae80413e48cf150633e30967d
Gabriel Mendes Boaventura <gabriel.boaventura@dinfor.uniceub>
2025-03-17 09:25:29 -0300

    Adicionando a primeira versão

---
+++ Projetos site RPG/Carrinho/MusicCar.js
@@ -0,0 +1,76 @@
+const audioPlayer = document.querySelector(".audio-player");
+const audio = new Audio("112. Penguin Race.mp3"); // Arquivo local no diretório "audio"
+
+audio.addEventListener(
+    "loadeddata",
+    () => {
+        audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum(
+            audio.duration
+        );
+        audio.volume = 0.75;
+    },
+    false
+);
+
+// Clique na timeline para pular
+const timeline = audioPlayer.querySelector(".timeline");
+timeline.addEventListener("click", e => {
+    const timelineWidth = window.getComputedStyle(timeline).width;
+    const timeToSeek = (e.offsetX / parseInt(timelineWidth)) * audio.duration;
+    audio.currentTime = timeToSeek;
+}, false);
+
+// Clique no volume slider para ajustar o volume
+const volumeSlider = audioPlayer.querySelector(".controls .volume-slider");
+volumeSlider.addEventListener("click", e => {
+    const sliderWidth = window.getComputedStyle(volumeSlider).width;
+    const newVolume = e.offsetX / parseInt(sliderWidth);
+    audio.volume = newVolume;
+    audioPlayer.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
+}, false);
+
+// Atualizar o progresso da música
+setInterval(() => {
+    const progressBar = audioPlayer.querySelector(".progress");
+    progressBar.style.width = (audio.currentTime / audio.duration) * 100 + "%";
+    audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum(
+        audio.currentTime
+    );
+}, 500);
+
+// Play/Pause
+const playBtn = audioPlayer.querySelector(".controls .toggle-play");
+playBtn.addEventListener(
+    "click",
+    () => {
+        if (audio.paused) {
+            playBtn.classList.remove("play");
+            playBtn.classList.add("pause");
+            audio.play();
+        } else {
+            playBtn.classList.remove("pause");
+            playBtn.classList.add("play");
+            audio.pause();
+        }
+    },
+    false
+);
+
+audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
+    const volumeEl = audioPlayer.querySelector(".volume-container .volume");
+    audio.muted = !audio.muted;
+    if (audio.muted) {
+        volumeEl.classList.remove("icono-volumeMedium");
+        volumeEl.classList.add("icono-volumeMute");
+    } else {
+        volumeEl.classList.add("icono-volumeMedium");
+        volumeEl.classList.remove("icono-volumeMute");
+    }
+});
+
+function getTimeCodeFromNum(num) {
+    let seconds = parseInt(num);
+    let minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+    return `${minutes}:${String(seconds).padStart(2, "0")}`;
+}
